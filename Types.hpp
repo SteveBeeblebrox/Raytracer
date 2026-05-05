7e5902a1b8bc9043f8cb4983ea60f402e98c55d0
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2026-05-05 15:52:46 -0600

    Prep for gpu

--- Types.hpp
+++ Types.hpp
@@ -15,6 +15,13 @@ struct Camera final {
     float vfov = mm::PI/4.0f;
 };
 
+struct Light final {
+    mm::vec3 pos = {5.0f, 5.0f, 5.0f};
+    mm::vec3 diffuse = {1.0f, 1.0f, 1.0f};
+    mm::vec3 ambient = {1.0f, 1.0f, 1.0f};
+    mm::vec3 specular = {1.0f, 1.0f, 1.0f};
+};
+
 struct Material final {
     mm::vec3 diffuse = {1.0f, 1.0f, 1.0f};
     mm::vec3 ambient = {0.2f, 0.2f, 0.2f};
@@ -26,29 +33,7 @@ struct Material final {
     float alpha = 1.0f;
 };
 
-struct Light final {
-    mm::vec3 pos = {5.0f, 5.0f, 5.0f};
-    mm::vec3 diffuse = {1.0f, 1.0f, 1.0f};
-    mm::vec3 ambient = {1.0f, 1.0f, 1.0f};
-    mm::vec3 specular = {1.0f, 1.0f, 1.0f};
-};
-
-struct Intersection final {
-    mm::vec3 pos;
-    mm::vec3 normal;
-    const Material* material = nullptr;
-    [[nodiscard]] inline bool is_valid() const {
-        return this->material != nullptr;
-    }
-
-    [[nodiscard]] static inline Intersection invalid() {
-        return Intersection {};
-    }
-
-    [[nodiscard]] static inline Intersection valid(const mm::vec3& pos, const mm::vec3& normal, const Material* material) {
-        return Intersection {pos, normal, material};
-    }
-};
+struct Intersection;
 
 struct Shape final {
     enum {PLANE, SPHERE} type;
@@ -64,40 +49,72 @@ struct Shape final {
         } as_plane;
     };
 
-    [[nodiscard]] inline Intersection intersect(const Ray& ray) const {
-        switch(this->type) {
-            case PLANE: {
-                float det, t;
-                if((det = mm::vec3::dot(ray.direction, this->as_plane.normal)) == 0) {
-                    // If origin is on plane
-                    if(mm::vec3::dot(ray.origin - this->as_plane.pos, this->as_plane.normal) == 0) {
-                        return Intersection::valid(ray.origin, this->as_plane.normal, &this->material);
-                    }
-                } else if((t = mm::vec3::dot(this->as_plane.pos - ray.origin, this->as_plane.normal/det)) >= 0.0f) {
-                        return Intersection::valid(ray.origin + t*ray.direction, this->as_plane.normal, &this->material);
-                }
-                return Intersection::invalid();
-            }
-            case SPHERE: {
-                const mm::vec3 x = ray.origin - this->as_sphere.pos;
-                const float a = mm::vec3::dot(ray.direction, ray.direction), b = 2.0f*mm::vec3::dot(x, ray.direction), c = mm::vec3::dot(x, x) - this->as_sphere.radius*this->as_sphere.radius;
-                
-                if(2.0f*a > 0.0f) {
-                    const float t1 = (-b + mm::sqrt(b*b - 4*a*c))/(2.0f*a), t2 = (-b - mm::sqrt(b*b - 4*a*c))/(2.0f*a);
+    __host__ __device__ [[nodiscard]] inline Intersection intersect(const Ray& ray) const;
+};
 
-                    if(t1 > 0.0f && t2 > 0.0f) {
-                        // If both positive, return smaller
-                        const mm::vec3 p = ray.origin + mm::min(t1, t2)*ray.direction;
-                        return Intersection::valid(p, mm::vec3::normalize(p - this->as_sphere.pos), &this->material);
-                    } else if(t1*t2 < 0.0f) {
-                        // If one is negative, return the other
-                        const mm::vec3 p = ray.origin + mm::max(t1, t2)*ray.direction;
-                        return Intersection::valid(p, mm::vec3::normalize(p - this->as_sphere.pos), &this->material);
-                    }
-                }
-                return Intersection::invalid();
-            }
+struct Intersection final {
+    mm::vec3 pos;
+    mm::vec3 normal;
+    const Material* material = nullptr;
+
+    __host__ __device__ [[nodiscard]] inline bool is_valid() const {
+        return this->material != nullptr;
+    }
+
+    __host__ __device__ [[nodiscard]] static inline Intersection invalid() {
+        return Intersection {};
+    }
+
+    __host__ __device__ [[nodiscard]] static inline Intersection valid(const mm::vec3& pos, const mm::vec3& normal, const Material* material) {
+        return Intersection {pos, normal, material};
+    }
+
+    __host__ __device__ [[nodiscard]] static Intersection closest(const mm::vec3& pos, const Intersection& a, const Intersection& b) {
+        return a.is_valid() && (!b.is_valid() || mm::vec3::distance(a.pos, pos) < mm::vec3::distance(b.pos, pos)) ? a : b;
+    }
+
+    __host__ __device__ [[nodiscard]] static Intersection of(const Ray& ray, const unsigned int shapec, const Shape* shapev) {
+        Intersection intersection = Intersection::invalid();
+        for(const Shape* shape = shapev; shape < shapev + shapec; shape++) {
+            intersection = Intersection::closest(ray.origin, shape->intersect(ray), intersection);
         }
-        return Intersection::invalid();
+        return intersection;
     }
 };
+
+__host__ __device__ inline Intersection Shape::intersect(const Ray& ray) const {
+    switch(this->type) {
+        case PLANE: {
+            float det, t;
+            if((det = mm::vec3::dot(ray.direction, this->as_plane.normal)) == 0) {
+                // If origin is on plane
+                if(mm::vec3::dot(ray.origin - this->as_plane.pos, this->as_plane.normal) == 0) {
+                    return Intersection::valid(ray.origin, this->as_plane.normal, &this->material);
+                }
+            } else if((t = mm::vec3::dot(this->as_plane.pos - ray.origin, this->as_plane.normal/det)) >= 0.0f) {
+                    return Intersection::valid(ray.origin + t*ray.direction, this->as_plane.normal, &this->material);
+            }
+            return Intersection::invalid();
+        }
+        case SPHERE: {
+            const mm::vec3 x = ray.origin - this->as_sphere.pos;
+            const float a = mm::vec3::dot(ray.direction, ray.direction), b = 2.0f*mm::vec3::dot(x, ray.direction), c = mm::vec3::dot(x, x) - this->as_sphere.radius*this->as_sphere.radius;
+            
+            if(2.0f*a > 0.0f) {
+                const float t1 = (-b + mm::sqrt(b*b - 4*a*c))/(2.0f*a), t2 = (-b - mm::sqrt(b*b - 4*a*c))/(2.0f*a);
+
+                if(t1 > 0.0f && t2 > 0.0f) {
+                    // If both positive, return smaller
+                    const mm::vec3 p = ray.origin + mm::min(t1, t2)*ray.direction;
+                    return Intersection::valid(p, mm::vec3::normalize(p - this->as_sphere.pos), &this->material);
+                } else if(t1*t2 < 0.0f) {
+                    // If one is negative, return the other
+                    const mm::vec3 p = ray.origin + mm::max(t1, t2)*ray.direction;
+                    return Intersection::valid(p, mm::vec3::normalize(p - this->as_sphere.pos), &this->material);
+                }
+            }
+            return Intersection::invalid();
+        }
+    }
+    return Intersection::invalid();
+}
